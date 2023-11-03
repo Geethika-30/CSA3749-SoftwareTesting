1336e407abdd885d647d4e8f245420a4cc27c7f1
dhanalakshmiramalingam <112678312+dhanalakshmiramalingam@users.noreply.github.com>
2023-11-03 13:53:53 +0530

    Delete EX1Test.java

--- EX1Test.java
+++
@@ -1,8 +0,0 @@
-public class EX1Test{
-
-    @Test
-    public void testReverseString() {
-        String reversedStr = EX1.reverseString("Hello");
-        assertEquals("olleH", reversedStr);
-    }
-}
