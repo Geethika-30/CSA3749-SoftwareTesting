ae66231058517c05d21b38a1f092ca5d0a102da7
dhanalakshmiramalingam <112678312+dhanalakshmiramalingam@users.noreply.github.com>
2023-11-16 08:57:43 +0530

    Update EX8TEST.java

--- Junit programs/EX8TEST.java
+++ Junit programs/EX8TEST.java
@@ -1,23 +1,16 @@
-import static org.junit.Assert.*;
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
-public class EX8TEST {
+public class EX8TEST{
 
     @Test
     public void testCalculateFactorial() {
-        EX8 calculator = new EX8();
+        assertEquals(1, FactorialCalculator.calculateFactorial(0));
+        assertEquals(120, FactorialCalculator.calculateFactorial(5));
+        assertEquals(3628800, FactorialCalculator.calculateFactorial(10));
 
-        assertEquals(1, calculator.calculateFactorial(0)); // 0! = 1
-        assertEquals(1, calculator.calculateFactorial(1)); // 1! = 1
-        assertEquals(2, calculator.calculateFactorial(2)); // 2! = 2
-        assertEquals(6, calculator.calculateFactorial(3)); // 3! = 6
-        assertEquals(24, calculator.calculateFactorial(4)); // 4! = 24
-        assertEquals(120, calculator.calculateFactorial(5)); // 5! = 120
-    }
-
-    @Test(expected = IllegalArgumentException.class)
-    public void testCalculateFactorialNegativeInput() {
-        EX8 calculator = new EX8();
-        calculator.calculateFactorial(-1);
+        assertThrows(IllegalArgumentException.class, () -> FactorialCalculator.calculateFactorial(-1));
+        // Add more test cases as needed
     }
 }
+                                                                    
