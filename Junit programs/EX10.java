af31192736fff16c95d6ce0d85f12322e41b32f9
dhanalakshmiramalingam <112678312+dhanalakshmiramalingam@users.noreply.github.com>
2023-11-16 09:10:28 +0530

    Update EX10.java

--- Junit programs/EX10.java
+++ Junit programs/EX10.java
@@ -1,9 +1,23 @@
-public class EX10{
-public static double calculateSquare(double number) {
-        return number * number;
+import java.util.Scanner;
+
+public class EX10 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Input decimal number
+        System.out.print("Enter a decimal number: ");
+        double decimalNumber = scanner.nextDouble();
+
+        // Calculate and display square and cube
+        System.out.println("Square: " + calculatePower(decimalNumber, 2));
+        System.out.println("Cube: " + calculatePower(decimalNumber, 3));
+
+        scanner.close();
     }
 
-    public static double calculateCube(double number) {
-        return number * number * number;
+    // Function to calculate power (square, cube, etc.)
+    public static double calculatePower(double number, int power) {
+        return Math.pow(number, power);
     }
 }
