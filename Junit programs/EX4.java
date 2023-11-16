96670759a4e25c55b87d0a626d0936d23e7412f5
dhanalakshmiramalingam <112678312+dhanalakshmiramalingam@users.noreply.github.com>
2023-11-16 08:40:04 +0530

    Update EX4.java

--- Junit programs/EX4.java
+++ Junit programs/EX4.java
@@ -1,10 +1,41 @@
-public class EX4{
+import java.util.Scanner;
 
-    public static double calculateSimpleInterest(double principal, double rate, double time) {
+public class EX4 {
+
+    
+    static double calculateSimpleInterest(double principal, double rate, double time) {
         return (principal * rate * time) / 100;
     }
 
+    
+    static double getRateOfInterest(boolean isSeniorCitizen) {
+        return isSeniorCitizen ? 12.0 : 10.0;
+    }
+
     public static void main(String[] args) {
-        // User input and output logic here
+        Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Enter the principal amount: ");
+        double principal = scanner.nextDouble();
+
+        
+        System.out.print("Enter the time (in years): ");
+        double time = scanner.nextDouble();
+
+        
+        System.out.print("Is the customer a senior citizen? (true/false): ");
+        boolean isSeniorCitizen = scanner.nextBoolean();
+
+       
+        double rate = getRateOfInterest(isSeniorCitizen);
+
+        
+        double simpleInterest = calculateSimpleInterest(principal, rate, time);
+
+        
+        System.out.println("Simple Interest: " + simpleInterest);
+
+        scanner.close();
     }
 }
